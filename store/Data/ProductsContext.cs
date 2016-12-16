b0aa11ce0d22c7fff7c58932860c5bf0e71656c5
Tomas Di Vito <tomasdv2@gmail.com>
2016-12-16 20:06:48 -0300

    fixed gitignore

--- store/Data/ProductsContext.cs
+++ store/Data/ProductsContext.cs
@@ -10,6 +10,6 @@ namespace store
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder
-                .UseMySql(@"Server=mysql5015.smarterasp.net;database=db_9f2d65_core;uid=9f2d65_core;pwd=qsc45plm87;");
+                .UseMySql(@"Server=mysql5015.smarterasp.net;database=ef;uid=9f2d65_core;pwd=19931101;");
     }
 }
